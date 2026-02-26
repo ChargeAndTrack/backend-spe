c67658dcd344e6c8e8feb101df1078ba464e96ab
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-02-26 16:18:11 +0100

    build: replace kotlin test with kotest dependencies and configure gradle libs.versions.toml file with versions, libraries, bundles and plugins

--- jvmapp/build.gradle.kts
+++ jvmapp/build.gradle.kts
@@ -1,10 +1,11 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.serialization") version "2.3.10"
     application
-    id("io.ktor.plugin") version "3.4.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotest)
+    alias(libs.plugins.serialization)
 }
 
 application {
@@ -22,19 +23,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-auth")
-    implementation("io.ktor:ktor-server-auth-jwt")
-    implementation(platform("org.mongodb:mongodb-driver-bom:5.6.1"))
-    implementation("org.mongodb:mongodb-driver-kotlin-coroutine")
-    implementation("org.mongodb:bson-kotlinx")
-
-    testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-client-cio")
-    testImplementation("io.ktor:ktor-client-content-negotiation")
+    implementation(libs.bundles.ktor.server)
+    implementation(libs.bundles.mongodb)
+    testImplementation(libs.bundles.kotlin.testing)
+    testImplementation(libs.bundles.ktor.testing)
 }
 
 kotlin {
