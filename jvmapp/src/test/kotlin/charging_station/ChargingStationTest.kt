11683ecfecf5a34f0352ea19f9b592ef2028c5d5
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-03-03 17:53:51 +0100

    test(charging-stations): refactor tests into contexts; add tests for failing operations; delete all charging stations before each context execution

--- jvmapp/src/test/kotlin/charging_station/ChargingStationTest.kt
+++ jvmapp/src/test/kotlin/charging_station/ChargingStationTest.kt
@@ -20,143 +20,218 @@ import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.put
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
 
-class ChargingStationTest : FunSpec({
+class ChargingStationTest : FunSpec() {
 
     lateinit var client: HttpClient
     lateinit var token: String
-    lateinit var completeChargingStation1: ChargingStationDTO
-    lateinit var completeChargingStation2: ChargingStationDTO
 
-    val chargingStation1 = AddChargingStationDTO(
-        power = 130,
-        location = LocationDTO(longitude = 50.44, latitude = 37.12)
-    )
-    val chargingStation2 = AddChargingStationDTO(
-        power = 80,
-        location = LocationDTO(longitude = 50.45, latitude = 37.15)
-    )
+    val chargingStation1 = AddChargingStationDTO(130, LocationDTO(longitude = 50.44, latitude = 37.12))
+    val chargingStation2 = AddChargingStationDTO(80, LocationDTO(longitude = 50.45, latitude = 37.15))
 
-    beforeSpec {
-        client = createClient()
-        token = loginAndGetToken(client, adminLoginDTO)
-        token.isNotEmpty() shouldBe true
-        completeChargingStation1 = client.post(chargingStationPath()) {
-            buildRequest(token, chargingStation1)
-        }.body<ChargingStationDTO>()
-        completeChargingStation2 = client.post(chargingStationPath()) {
-            buildRequest(token, chargingStation2)
-        }.body<ChargingStationDTO>()
-    }
-
-    afterSpec {
-        client.close()
-    }
-
-    test("get nearby charging stations test") {
-        val response = client.get(chargingStationPath("near")) {
-            buildRequest<Unit>(token, parametersMap = mapOf(
-                "lng" to 50.44.toString(),
-                "lat" to 37.12.toString(),
-                "radius" to 50_000.0.toString()
-            ))
+    init {
+        beforeSpec {
+            client = createClient()
+            token = loginAndGetToken(client, adminLoginDTO)
+            token.isNotEmpty() shouldBe true
         }
-        response.status shouldBeEqual HttpStatusCode.OK
-        response.body<Collection<ChargingStationDTO>>() shouldContainExactly listOf(
-            completeChargingStation1,
-            completeChargingStation2
-        )
-    }
 
-    test("get closest charging station test") {
-        client.put(chargingStationPath(completeChargingStation1._id ?: "")) {
-            buildRequest(
-                token,
-                UpdateChargingStationDTO(
-                    power = null,
-                    enabled = false,
-                    available = null,
-                    location = null
+        afterSpec { client.close() }
+
+        beforeEach { deleteAllChargingStations() }
+
+        test("it should list all charging stations successfully") {
+            val chargingStations = insertChargingStations()
+            val response = client.get(chargingStationPath()) { buildRequest<Unit>(token) }
+            response.status shouldBeEqual HttpStatusCode.OK
+            response.body<Collection<ChargingStationDTO>>() shouldContainExactly chargingStations
+        }
+
+        context("add charging station tests") {
+            test("it should add a charging station successfully") {
+                val response = insertChargingStation(chargingStation1)
+                val responseBody = response.body<ChargingStationDTO>()
+                response.status shouldBeEqual HttpStatusCode.Created
+                responseBody.power shouldBeEqual chargingStation1.power
+                responseBody.location shouldBeEqual chargingStation1.location
+            }
+
+            test("it should fail to add a charging station with non-positive power") {
+                val addChargingStation = AddChargingStationDTO(
+                    0,
+                    LocationDTO(longitude = 50.44, latitude = 37.12)
                 )
-            )
+                insertChargingStation(addChargingStation).status shouldBeEqual HttpStatusCode.BadRequest
+                insertChargingStation(addChargingStation.copy(power = -10))
+                    .status shouldBeEqual HttpStatusCode.BadRequest
+            }
         }
-        val response = client.get(chargingStationPath("closest")) {
-            buildRequest<Unit>(token, parametersMap = mapOf(
-                "lng" to 50.44.toString(),
-                "lat" to 37.00.toString(),
-                "onlyEnabledAndAvailable" to true.toString()
-            ))
+
+        context("get charging station tests") {
+            lateinit var chargingStation: ChargingStationDTO
+
+            beforeEach { chargingStation = insertChargingStation(chargingStation1).body<ChargingStationDTO>() }
+
+            test("it should get the charging station by id successfully") {
+                val response = client.get(chargingStationPath(chargingStation._id ?: "")) {
+                    buildRequest<Unit>(token)
+                }
+                response.status shouldBeEqual HttpStatusCode.OK
+                response.body<ChargingStationDTO>() shouldBeEqual chargingStation
+            }
+
+            test("it should fail to get the charging station with a wrong id") {
+                client.get(chargingStationPath("")) {
+                    buildRequest<Unit>(token)
+                }.status shouldBeEqual HttpStatusCode.NotFound
+            }
         }
-        response.status shouldBeEqual HttpStatusCode.OK
-        response.body<ChargingStationDTO>() shouldBeEqual completeChargingStation2
+
+        context("update charging station tests") {
+            val updateChargingStation = UpdateChargingStationDTO(power = 150, available = false)
+            lateinit var chargingStation: ChargingStationDTO
+
+            beforeEach { chargingStation = insertChargingStation(chargingStation1).body<ChargingStationDTO>() }
+
+            test("it should update the charging station by id successfully") {
+                val response = client.put(chargingStationPath(chargingStation._id ?: "")) {
+                    buildRequest(token, updateChargingStation)
+                }
+                response.status shouldBeEqual HttpStatusCode.OK
+                response.body<ChargingStationDTO>() shouldBeEqual ChargingStationDTO(
+                    _id = chargingStation._id,
+                    power = updateChargingStation.power ?: chargingStation.power,
+                    available = updateChargingStation.available ?: chargingStation.available,
+                    enabled = updateChargingStation.enabled ?: chargingStation.enabled,
+                    location = updateChargingStation.location ?: chargingStation.location
+                )
+            }
+
+            test("it should fail to update the charging station with a wrong id") {
+                client.put(chargingStationPath("")) {
+                    buildRequest(token, updateChargingStation)
+                }.status shouldBeEqual HttpStatusCode.NotFound
+            }
+
+            test("it should fail to update the charging station with a non-positive power") {
+                client.put(chargingStationPath(chargingStation._id ?: "")) {
+                    buildRequest(token, updateChargingStation.copy(power = 0))
+                }.status shouldBeEqual HttpStatusCode.BadRequest
+                client.put(chargingStationPath(chargingStation._id ?: "")) {
+                    buildRequest(token, updateChargingStation.copy(power = -10))
+                }.status shouldBeEqual HttpStatusCode.BadRequest
+            }
+        }
+
+        context("delete charging station tests") {
+            lateinit var chargingStation: ChargingStationDTO
+
+            beforeEach { chargingStation = insertChargingStation(chargingStation1).body<ChargingStationDTO>() }
+
+            test("it should delete the charging station successfully") {
+                client.delete(chargingStationPath(chargingStation._id ?: "")) {
+                    buildRequest<Unit>(token)
+                }.status shouldBeEqual HttpStatusCode.OK
+            }
+
+            test("it should fail to delete the charging station with wrong id") {
+                client.delete(chargingStationPath("")) {
+                    buildRequest<Unit>(token)
+                }.status shouldBeEqual HttpStatusCode.NotFound
+            }
+        }
+
+        context("get charging stations by position tests") {
+            lateinit var chargingStations: Collection<ChargingStationDTO>
+
+            beforeEach { chargingStations = insertChargingStations() }
+
+            test("it get near charging stations successfully") {
+                val response = client.get(chargingStationPath("near")) {
+                    buildRequest<Unit>(token, parametersMap = mapOf(
+                        "lng" to 50.44.toString(),
+                        "lat" to 37.12.toString(),
+                        "radius" to 50_000.0.toString()
+                    ))
+                }
+                response.status shouldBeEqual HttpStatusCode.OK
+                response.body<Collection<ChargingStationDTO>>() shouldContainExactly chargingStations
+            }
+
+            test("it should fail to get the near charging stations and the closest one when invalid coordinates are passed") {
+                client.get(chargingStationPath("near")) {
+                    buildRequest<Unit>(token, parametersMap = mapOf(
+                        "lng" to (-182.44).toString(),
+                        "lat" to 94.12.toString(),
+                        "radius" to 50_000.0.toString()
+                    ))
+                }.status shouldBeEqual HttpStatusCode.BadRequest
+                client.get(chargingStationPath("closest")) {
+                    buildRequest<Unit>(token, parametersMap = mapOf(
+                        "lng" to (-182.44).toString(),
+                        "lat" to 94.12.toString(),
+                        "onlyEnabledAndAvailable" to true.toString()
+                    ))
+                }.status shouldBeEqual HttpStatusCode.BadRequest
+            }
+
+            test("it should fail to get the near charging stations when a negative radius is passed") {
+                client.get(chargingStationPath("near")) {
+                    buildRequest<Unit>(token, parametersMap = mapOf(
+                        "lng" to 50.44.toString(),
+                        "lat" to 37.12.toString(),
+                        "radius" to (-10.0).toString()
+                    ))
+                }.status shouldBeEqual HttpStatusCode.BadRequest
+            }
+
+            test("it should get closest charging station successfully") {
+                val response1 = client.get(chargingStationPath("closest")) {
+                    buildRequest<Unit>(token, parametersMap = mapOf(
+                        "lng" to 50.44.toString(),
+                        "lat" to 37.00.toString()
+                    ))
+                }
+                client.put(chargingStationPath(chargingStations.first()._id ?: "")) {
+                    buildRequest(token, UpdateChargingStationDTO(enabled = false))
+                }
+                val response2 = client.get(chargingStationPath("closest")) {
+                    buildRequest<Unit>(
+                        token,
+                        parametersMap = mapOf(
+                            "lng" to 50.44.toString(),
+                            "lat" to 37.00.toString(),
+                            "onlyEnabledAndAvailable" to true.toString()
+                        )
+                    )
+                }
+                response1.status shouldBeEqual HttpStatusCode.OK
+                response1.body<ChargingStationDTO>() shouldBeEqual chargingStations.first()
+                response2.status shouldBeEqual HttpStatusCode.OK
+                response2.body<ChargingStationDTO>() shouldBeEqual chargingStations.elementAt(1)
+            }
+        }
     }
 
-    test("add charging station test") {
-        val response = client.post(chargingStationPath()) {
-            buildRequest(token, chargingStation1)
-        }
-        val responseBody = response.body<ChargingStationDTO>()
-        completeChargingStation1 = responseBody
-        response.status shouldBeEqual HttpStatusCode.Created
-        responseBody.power shouldBeEqual chargingStation1.power
-        responseBody.location shouldBeEqual chargingStation1.location
-    }
+    private fun chargingStationPath(vararg paths: String) = BASE_URL.assemblePath("charging-stations", *paths)
 
-    test("list all charging stations test") {
-        val response = client.get(chargingStationPath()) {
-            buildRequest<Unit>(token)
-        }
-        response.status shouldBeEqual HttpStatusCode.OK
-        response
+    private suspend fun deleteAllChargingStations() {
+        val chargingStations = client.get(chargingStationPath()) { buildRequest<Unit>(token) }
             .body<Collection<ChargingStationDTO>>()
-            .find { it._id == completeChargingStation1._id } shouldBe completeChargingStation1
+        chargingStations.forEach {
+            client.delete(chargingStationPath(it._id ?: "")) { buildRequest<Unit>(token) }
+        }
     }
 
-    test("get charging station by id test") {
-        val response = client.get(chargingStationPath(completeChargingStation1._id ?: "")) {
-            buildRequest<Unit>(token)
+    private suspend fun insertChargingStation(chargingStationToAdd: AddChargingStationDTO): HttpResponse =
+        client.post(chargingStationPath()) {
+            buildRequest(token, chargingStationToAdd)
         }
-        val wrongIdResponse = client.get(chargingStationPath("")) {
-            buildRequest<Unit>(token)
-        }
-        val responseBody = response.body<ChargingStationDTO>()
-        response.status shouldBeEqual HttpStatusCode.OK
-        completeChargingStation1 shouldBeEqual responseBody
-        wrongIdResponse.status shouldBeEqual HttpStatusCode.NotFound
-    }
 
-    test("update charging station by id test") {
-        val updateChargingStation = UpdateChargingStationDTO(
-            power = 150,
-            available = false,
-            enabled = null,
-            location = null
-        )
-        val response = client.put(chargingStationPath(completeChargingStation1._id ?: "")) {
-            buildRequest(token, updateChargingStation)
-        }
-        val expectedResult = ChargingStationDTO(
-            _id = completeChargingStation1._id,
-            power = updateChargingStation.power ?: completeChargingStation1.power,
-            available = updateChargingStation.available ?: completeChargingStation1.available,
-            enabled = updateChargingStation.enabled ?: completeChargingStation1.enabled,
-            location = updateChargingStation.location ?: completeChargingStation1.location
-        )
-        response.status shouldBeEqual HttpStatusCode.OK
-        response.body<ChargingStationDTO>() shouldBeEqual expectedResult
-    }
-
-    test("delete charging station by id test") {
-        val response = client.delete(chargingStationPath(completeChargingStation1._id ?: "")) {
-            buildRequest<Unit>(token)
-        }
-        val wrongResponse = client.delete(chargingStationPath("")) {
-            buildRequest<Unit>(token)
-        }
-        response.status shouldBeEqual HttpStatusCode.OK
-        wrongResponse.status shouldBeEqual HttpStatusCode.NotFound
-    }
-})
-
-private fun chargingStationPath(vararg paths: String) = BASE_URL.assemblePath("charging-stations", *paths)
+    private suspend fun insertChargingStations(): Collection<ChargingStationDTO> = listOf(
+        insertChargingStation(chargingStation1),
+        insertChargingStation(chargingStation2)
+    ).map { it.body<ChargingStationDTO>() }.toList()
+}
