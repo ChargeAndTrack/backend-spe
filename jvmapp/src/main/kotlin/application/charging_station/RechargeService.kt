e229d73e34bd38af644dc46f884bd4cf68a4b1e7
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2026-03-11 17:56:37 +0100

    feat(recharge): add StartRechargeLogicDTO; change start and stop recharge methods using a mutable map of active recharges and implement notifyRechargeEvent method because now RechargeService is a RechargeObserver

--- jvmapp/src/main/kotlin/application/charging_station/RechargeService.kt
+++ jvmapp/src/main/kotlin/application/charging_station/RechargeService.kt
@@ -1,15 +1,22 @@
 package application.charging_station
 
+import domain.charging_station.RechargeObserver
+import domain.charging_station.StartRechargeLogicInput
 import domain.charging_station.StartRechargeInput
 import domain.charging_station.StopRechargeInput
 
-interface RechargeService {
+interface RechargeService : RechargeObserver {
 
     suspend fun getChargingStationIdByCarId(carId: String): String
 
     suspend fun getCarIdByChargingStationId(chargingStationId: String): String
 
-    suspend fun startRecharge(startRechargeInput: StartRechargeInput, chargingStationId: String)
+    suspend fun startRecharge(
+        userId: String,
+        startRechargeInput: StartRechargeInput,
+        startRechargeLogicInput: StartRechargeLogicInput,
+        chargingStationId: String
+    )
 
     suspend fun stopRecharge(stopRechargeInput: StopRechargeInput, chargingStationId: String)
 }
--- jvmapp/src/main/kotlin/infrastructure/charging_station/RechargeController.kt
+++ jvmapp/src/main/kotlin/infrastructure/charging_station/RechargeController.kt
@@ -1,22 +1,38 @@
 package infrastructure.charging_station
 
-import application.charging_station.RechargeService
+import application.charging_station.ChargingStationServiceImpl
 import application.charging_station.RechargeServiceImpl
+import application.user.CarServiceImpl
+import infrastructure.user.MongoDbUserRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.principal
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 
 object RechargeController {
 
-    val rechargeService: RechargeService = RechargeServiceImpl(MongoDbRechargeRepository())
+    private val chargingStationService = ChargingStationServiceImpl(MongoDbChargingStationRepository())
+    private val carService = CarServiceImpl(MongoDbUserRepository())
+    private val rechargeService = RechargeServiceImpl(MongoDbRechargeRepository(), chargingStationService, carService)
 
     suspend fun startRecharge(call: ApplicationCall) = call.handleRechargeRequest { chargingStationId ->
         println("Start recharge")
-        ChargingStationsController.chargingStationService.getChargingStation(chargingStationId).validateRecharge()
+        val request = call.receive<StartRechargeDTO>()
+        val chargingStation = chargingStationService.getChargingStation(chargingStationId)
+        chargingStation.validateRecharge()
         call.respond(
             HttpStatusCode.OK,
-            rechargeService.startRecharge(call.receive<StartRechargeDTO>().toInput(), chargingStationId)
+            rechargeService.startRecharge(
+                call.getUserId(),
+                request.toInput(),
+                StartRechargeLogicDTO(
+                    chargingStation.power,
+                    carService.getCar(call.getUserId(), request.carId).maxBattery
+                ).toInput(),
+                chargingStationId
+            )
         )
     }
 
@@ -27,6 +43,8 @@ object RechargeController {
             rechargeService.stopRecharge(call.receive<StopRechargeDTO>().toInput(), chargingStationId)
         )
     }
+
+    private fun ApplicationCall.getUserId() = principal<JWTPrincipal>()!!.payload.getClaim("userId").asString()
 
     private suspend fun ApplicationCall.handleRechargeRequest(
         handler: suspend (chargingStationId: String) -> Unit
